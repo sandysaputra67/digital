20188ed23cc46be3dd012377777a5093d9f8f4c4
Galih Nur Aditya <galihnuraditya@gmail.com>
2021-04-26 13:28:52 +0700

    chore: create homepage layout

---
+++ resources/views/home.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Digital Menu</title>
+
+    <link rel="stylesheet" href="{{ asset('assets/css/tailwind.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/home.css') }}">
+</head>
+
+<body class="bg-green-200">
+    <main class="text-center">
+        <h4 class="inline-block mt-60 border-2 font-medium p-2 border-green-800 text-5xl text-green-800">MENU</h4>
+    </main>
+
+    <div class="footer w-full block absolute bottom-0 text-center">
+        <div class="resto-name mb-24">
+            <h4 class="text-center text-3xl text-green-800 mb-2 font-medium">Nama Rumah Makan</h4>
+            <hr class="bg-green-800 mx-auto w-28 text-center" />
+        </div>
+
+        <div class="start-button absolute bottom-0 left-0 p-8 w-full bg-gradient-to-t">
+            <div class=""></div>
+            <a href="#" class="text-xl text-white">Klik untuk memulai</a>
+        </div>
+    </div>
+</body>
+
+</html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+mix.js('resources/js/app.js', 'public/assets/js')
+    .postCss('resources/css/tailwind.css', 'public/assets/css', [
+        require("tailwindcss"),
+    ])
+    .sass('resources/sass/app.scss', 'public/assets/css');
