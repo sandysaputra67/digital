9136433d16fb1a0ac38dcddb5eaec152569e5232
plgtwn <userpras@gmail.com>
2021-04-30 16:59:34 +0700

    menambahkan fungsi memanggil menu dan update
    - Memindahkan fungsi reservasi dari table controller ke cart controller

--- app/Http/Controllers/Restaurant/MenuController.php
+++ app/Http/Controllers/Restaurant/MenuController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Restaurant;
 
 use App\Http\Controllers\Controller;
 use App\Models\Restaurant;
+use App\Restaurant\GroupMenus;
 use App\Restaurant\Menu;
 use Illuminate\Http\Request;
 
@@ -17,5 +18,36 @@ class MenuController extends Controller
     public function show(Restaurant $restaurant, $menu)
     {
         return view('restaurant.pages.menu.show', compact('restaurant', 'menu'));
+    }
+
+    public function getMenus(Restaurant $restaurant)
+    {
+        $all_menus = Menu::where([
+            'restaurant_id' => $restaurant->id
+        ])->get();
+
+        $group_menus = GroupMenus::where([
+            'restaurant_id' => $restaurant->id,
+            'main' => true
+        ])->first();
+
+        if ($group_menus) {
+            $featured_menu = $all_menus->where('group_id', $group_menus->id)->get();
+        }
+
+        $menu_with_image = $all_menus->whereNotNull('image',)->get();
+
+        $menu_only_text = $all_menus->whereNull('image',)->get();
+
+        $data = [
+            'featured_menus' => $featured_menu,
+            'menu_with_image' => $menu_with_image,
+            'menu_only_text' => $menu_only_text,
+        ];
+
+        return response()->json([
+            'status' => 'OK',
+            'data_menu' => $data
+        ])
     }
 }
---
+++ database/migrations/2021_04_30_034653_create_cart_items_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCartItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cart_items', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('cart_id');
+            $table->bigInteger('menu_id');
+            $table->integer('quantity')->nullable();
+            $table->decimal('base_total', 12, 4)->default(0);
+            $table->decimal('discount_amount', 12, 4)->default(0);
+            $table->string('note')->nullable();
+            $table->string('status')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cart_items');
+    }
+}
